16b30f2e0047ec6ec3c536c61bf6d81c3de2a6e1
Levi14ram <75736380@certus.edu.pe>
2022-06-14 00:59:21 -0500

    Update proPersonas.dart
    preliminar de funciones filtradas de firestore

--- lib/Separaciones/Cliente/proPersonas.dart
+++ lib/Separaciones/Cliente/proPersonas.dart
@@ -1,7 +1,17 @@
 // ignore_for_file: file_names
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:workbot_servicios/victor_perfil.dart';
 import 'package:workbot_servicios/vistaTrabajador.dart';
+
+void main() {
+  WidgetsFlutterBinding
+      .ensureInitialized(); //se asegura que todas las dependencias de fluter esten inicilaizadas antes de comenzar
+  Firebase.initializeApp().then((value) {
+    runApp(Prueba());
+  });
+}
 
 class Prueba extends StatelessWidget {
   const Prueba({Key? key}) : super(key: key);
@@ -27,6 +37,175 @@ class Cuerpo extends StatefulWidget {
 }
 
 class _CuerpoState extends State<Cuerpo> {
+//varibles q contienen los datos filtrados
+
+  List Electrisista = [];
+  List Gasfitero = [];
+  List Soldador = [];
+  List Mecanico = [];
+  List Carpintero = [];
+  List Vidreria = [];
+  List Fontaner = [];
+  List Delivery = [];
+  List Contador = [];
+  List Abogado = [];
+  List Odontologia = [];
+  List Fisioterapeuta = [];
+  List Enfermeria = [];
+  List Veterinario = [];
+  List Barman = [];
+  List Animador = [];
+  List Dj = [];
+  List Pediatra = [];
+  List Asesoramiento = [];
+  List Jardinero = [];
+  List Cheff = [];
+  List Barbershop = [];
+  List Cantante = [];
+  List Fotografo = [];
+  List Pintor = [];
+
+//..........................................
+  //funciones para traer las data desde firestore
+  @override
+  void initState() {
+    getUserspintor();
+    getUserselectrisista();
+    getUsersgasfitero();
+    getUsersoldador();
+    getUsermecanico();
+    getUsercarpintero();
+    super.initState();
+  }
+
+  void getUserspintor() async {
+    //funcion que trae los datos filtrados usuarios pintores
+    List userinit = [];
+
+    CollectionReference coleccionDataFirebase =
+        FirebaseFirestore.instance.collection("colaboradores");
+    QuerySnapshot users =
+        await coleccionDataFirebase.where("jobs", isEqualTo: 'pintor').get();
+
+    if (users.docs.length != 0) {
+      for (var doc in users.docs) {
+        userinit.add(doc.data());
+        print('antes de :..........$userinit');
+      }
+    }
+    Pintor.add(userinit.map((user) => user['names'] + ' ' + user['surnames']));
+    print('El despued es :.......$Pintor');
+  }
+
+//..........................................................
+  void getUserselectrisista() async {
+    //funcion que trae los datos filtrados usuarios electrisistas
+    List userinit = [];
+
+    CollectionReference coleccionDataFirebase =
+        FirebaseFirestore.instance.collection("colaboradores");
+    QuerySnapshot users = await coleccionDataFirebase
+        .where("jobs", isEqualTo: 'electrisista')
+        .get();
+
+    if (users.docs.length != 0) {
+      for (var doc in users.docs) {
+        userinit.add(doc.data());
+        print('antes de :..........$userinit');
+      }
+    }
+    Electrisista.add(
+        userinit.map((user) => user['names'] + ' ' + user['surnames']));
+    print('El despued es :.......$Electrisista');
+  }
+
+  //..............................................
+  void getUsersgasfitero() async {
+    //funcion que trae los datos filtrados usuarios gasfitero
+    List userinit = [];
+
+    CollectionReference coleccionDataFirebase =
+        FirebaseFirestore.instance.collection("colaboradores");
+    QuerySnapshot users =
+        await coleccionDataFirebase.where("jobs", isEqualTo: 'gasfitero').get();
+
+    if (users.docs.length != 0) {
+      for (var doc in users.docs) {
+        userinit.add(doc.data());
+        print('antes de :..........$userinit');
+      }
+    }
+    Gasfitero.add(
+        userinit.map((user) => user['names'] + ' ' + user['surnames']));
+    print('El despued es :.......$Gasfitero');
+  }
+
+  //.........................................
+  void getUsersoldador() async {
+    //funcion que trae los datos filtrados usuarios soldador
+    List userinit = [];
+
+    CollectionReference coleccionDataFirebase =
+        FirebaseFirestore.instance.collection("colaboradores");
+    QuerySnapshot users =
+        await coleccionDataFirebase.where("jobs", isEqualTo: 'soldador').get();
+
+    if (users.docs.length != 0) {
+      for (var doc in users.docs) {
+        userinit.add(doc.data());
+        print('antes de :..........$userinit');
+      }
+    }
+    Soldador.add(
+        userinit.map((user) => user['names'] + ' ' + user['surnames']));
+    print('El despued es :.......$Soldador');
+  }
+
+  //.......................................
+  void getUsermecanico() async {
+    //funcion que trae los datos filtrados usuarios mecanico
+    List userinit = [];
+
+    CollectionReference coleccionDataFirebase =
+        FirebaseFirestore.instance.collection("colaboradores");
+    QuerySnapshot users =
+        await coleccionDataFirebase.where("jobs", isEqualTo: 'mecanico').get();
+
+    if (users.docs.length != 0) {
+      for (var doc in users.docs) {
+        userinit.add(doc.data());
+        print('antes de :..........$userinit');
+      }
+    }
+    Mecanico.add(
+        userinit.map((user) => user['names'] + ' ' + user['surnames']));
+    print('El despued es :.......$Mecanico');
+  }
+
+  //.....................................
+  void getUsercarpintero() async {
+    //funcion que trae los datos filtrados usuarios mecanico
+    List userinit = [];
+
+    CollectionReference coleccionDataFirebase =
+        FirebaseFirestore.instance.collection("colaboradores");
+    QuerySnapshot users = await coleccionDataFirebase
+        .where("jobs", isEqualTo: 'carpintero')
+        .get();
+
+    if (users.docs.length != 0) {
+      for (var doc in users.docs) {
+        userinit.add(doc.data());
+        print('antes de :..........$userinit');
+      }
+    }
+    Carpintero.add(
+        userinit.map((user) => user['names'] + ' ' + user['surnames']));
+    print('El despued es :.......$Carpintero');
+  }
+  //
+  //------------------------------------
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
